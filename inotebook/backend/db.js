cd210185b36edc7c9aa964107c43e2c721fecfac
AbhishekSingh971 <abhitil654@gmail.com>
2022-08-25 12:04:21 +0530

    Delete inotebook directory

--- inotebook/backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const monoURI = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.1tynyfo.mongodb.net/test"
-const connectToMongo = ()=>{
-    mongoose.connect(monoURI, ()=>{
-        console.log("connect to mongo successfully")
-    })
-}
-
-module.exports = connectToMongo;
