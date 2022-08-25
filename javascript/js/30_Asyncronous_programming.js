3d899aec7639de33773eff3bde14946c8f10eec9
AbhishekSingh971 <abhitil654@gmail.com>
2022-08-25 12:01:05 +0530

    Delete javascript directory

--- javascript/js/30_Asyncronous_programming.js
+++
@@ -1,6 +0,0 @@
-setTimeout(() => {for (let index = 0; index < 6000; index++) {
-    const element = index;
-    console.log('This is index number' + index);
-}
-}, 100);
-console.log("done printing");
