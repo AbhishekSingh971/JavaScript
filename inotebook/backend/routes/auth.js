c945c99a0fb253e4ecb466744d0f613c83f4f584
Abhi5hek5ingh <abhitil654@gmail.com>
2022-08-27 00:02:19 +0530

    video 49 completed with token, hashing, slat and peper

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Thisis$token';
 
 //Create a User using: POST "/api/auth/createuser". no login required
 //if you want to use req.body, than you have to use middel wayer in index.js
@@ -15,28 +19,38 @@ router.post(
       max: 16,
     }),
   ],
-  (req, res) => {
+   async (req, res) => {
     // if there are errors , return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    // Check whether the user with this email exist already
     try{//idealy we don/t use try and catch ,we put it in Logger or SQS 
+      // Check whether the user with this email exist already
+      const salt =await bcrypt.genSalt(10);
+      secPass = await bcrypt.hash(req.body.password, salt)
 
-    let user = user.findOne({email: req.body.email});
-    if(user){
-      return res.status(400).json({error: "Sorry a user with this email already exist"});
-    }
+       // Create a new User
+       user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      const data = {
+        user: {id: user.id}
+      }
 
-    // Create a new User
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      res.json(user)
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      console.log(jwtData);
 
+        // res.json(user)
+        res.json({authtoken});
+      
+      let user = user.findOne({email: req.body.email});
+      if(user){
+        return res.status(400).json({error: "Sorry a user with this email already exist"});
+      }
+  
 }catch(error){
   console.error(error.message)
   res.status(500).send("some error occured");
