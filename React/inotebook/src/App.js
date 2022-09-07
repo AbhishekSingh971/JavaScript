e044859ff25e7bdacaca375bbcb93fff489650bf
AbhishekSingh971 <abhitil654@gmail.com>
2022-09-08 03:01:58 +0530

    video 58 ,useContext hook completed

--- React/inotebook/src/App.js
+++ React/inotebook/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import NoteState from './Context/notes/NoteState';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello react app</h1>
-    </div>
+    <NoteState>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path = "/">
+            <Home/>
+          </Route>
+          <Route exact path = "/about">
+            <About/>
+          </Route>
+        </Switch>
+      </Router>
+    </NoteState>
   );
 }
 
